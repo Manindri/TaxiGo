e02ab255fa47b5633f90e56085a0c39a54c60b84
Manindri <IT20246778@my.sliit.lk>
2023-09-07 19:09:43 +0530

    init

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,20 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2></h2>
+      <p>
+        Welcome to TaxiGo, your trusted destination for hassle-free taxi
+        bookings! Say goodbye to the stress of finding a ride in a crowded city
+        or waiting endlessly for transportation.
+      </p>
+      <p>
+        With TaxiGo, you're just a click away from securing a comfortable,
+        reliable, and affordable taxi ride to your destination.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
---
+++ src/components/Meals/dummy-meals.js
@@ -0,0 +1,24 @@
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Bike",
+    price: 200.0
+  },
+  {
+    id: "m2",
+    name: "Three wheeler",
+    price: 350.0
+  },
+  {
+    id: "m3",
+    name: "Car",
+    price: 500.0
+  },
+  {
+    id: "m4",
+    name: "SUV",
+    price: 700.0
+  }
+];
+
+export default DUMMY_MEALS;
